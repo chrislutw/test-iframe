2c2de7dedae1bba4f40abafd68e0d8596cf931f1
chrislutw <kshke0723@gmail.com>
2022-12-21 19:14:26 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- <div>
+    <NuxtWelcome />
+  </div> -->
+  <div class="h-screen w-full auto-rows-min grid gap-4 grid-cols-4 grid-rows-4 justify-center items-center">
+    <iframe width="100%" height="100%" class="col-span-4 row-span-2"
+      src="https://test-wbgame.jlfafafa3.com/gl_sl/index.html" frameborder="0" />
+    <iframe width="100%" height="100%" class="col-span-2" src="https://test-wbgame.jlfafafa3.com/gl_sl/index.html"
+      frameborder="0" />
+    <iframe width="100%" height="100%" class="col-span-2 row-span-2"
+      src="https://test-wbgame.jlfafafa3.com/gl_sl/index.html" frameborder="0" />
+  </div>
+
+</template>
