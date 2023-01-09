be1c729420aab2aaa99f7d9f1a6980d10d39a60d
chrislutw <kshke0723@gmail.com>
2023-01-09 18:40:20 +0800

    update animate to pirate

--- pages/spine-demo.vue
+++ pages/spine-demo.vue
@@ -6,43 +6,66 @@ onMounted(() => {
   console.log("Hello World")
   initSpine()
 })
+const player: any = ref(null)
 
 function initSpine() {
   // @ts-ignore
-  new spine.SpinePlayer("player-container", {
-    jsonUrl: "/mySpine/skeleton.json",
-    atlasUrl: "/mySpine/skeleton.atlas",
+  const playerInstance = new spine.SpinePlayer("player-container", {
+    jsonUrl: "/pirate/pirate.json",
+    atlasUrl: "/pirate/pirate.atlas",
+    // audioFolderPath: "/pirate/audio",
+    // premultipliedAlpha: false,
+    // animation: "idle",
+    animations: ['idle', "attack"],
     showControls: true,
     backgroundColor: "#00000000",
     alpha: true,
     // Added:
-    success: (player: any) => {
-      console.log(player);
-      // player.animationState.addListener({
-      //   event: function (t: any) {
-      //     switch (t.animation.name) {
-      //       case "surf":
-      //         player.animationState.setAnimation(0, "jump", true);
-      //         onToOff();
-      //         break;
-      //       case "jump":
-      //         player.animationState.setAnimation(0, "Skateboard", true);
-      //         break;
-      //       case "jump2":
-      //         player.animationState.setAnimation(0, "surf", true);
-      //         break;
-      //       case "Skateboard":
-      //         player.animationState.setAnimation(0, "jump2", true);
-      //         break;
-      //     }
-      //   }
-      // });
-      // player.animationState.setAnimation(0, "animation", true);
-    }
+    // success: (player: any) => {
+    //   console.log('success: ', player)
+    //   player.setAnimation("attack", true)
+    //   player.animationState.setAnimation(0, "attack");
+    //   player.animationState.addListener({
+    //     event(t: any) {
+    //       console.log('addListener: ', t.animation.name)
+    //       switch (t.animation.name) {
+    //         case "attack":
+    //           player.animationState.setAnimation(0, "attack");
+    //           break;
+    //         case "idle":
+    //           player.animationState.setAnimation(0, "idle", true);
+    //           break;
+    //       }
+    //     }
+    //   })
+    // }
   })
+  // setTimeout(() => {
+  //   playerInstance.setAnimation("attack", true)
+  //   playerInstance.animationState?.setAnimation(0, "attack", true)
+
+  // }, 2000);
+  // playerInstance.setAnimation('idle', true)
+  player.value = playerInstance
+}
+function attack() {
+  console.log('chage to attack')
+  player.value.setAnimation("attack")
+}
+function stop() {
+  console.log('stop')
+  player.value.animationState.setEmptyAnimation(0);
 }
 </script>
 
 <template>
-  <div id="player-container" />
+  <div class="bg-red-500">
+    <div id="player-container" />
+    <button class="rounded bg-blue-400 p-2" @click="attack()">
+      player.setAnimation('attack')
+    </button>
+    <button class="rounded bg-blue-400 p-2" @click="stop()">
+      stop
+    </button>
+  </div>
 </template>
