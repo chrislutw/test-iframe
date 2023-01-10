a9d403cb785e609c8f676720c2bf721c0e7628ab
chrislutw <kshke0723@gmail.com>
2023-01-10 18:26:20 +0800

    config animate bind sound

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   },
   modules: [
     '@unocss/nuxt',
+    '@vueuse/nuxt',
   ],
   css: [
     '@unocss/reset/tailwind.css',
--- pages/spine-demo.vue
+++ pages/spine-demo.vue
@@ -1,10 +1,36 @@
 <script lang="ts" setup>
 import * as spine from "@esotericsoftware/spine-player"
 import "@esotericsoftware/spine-player/dist/spine-player.css"
+import { Howl, Howler } from 'howler'
 
-onMounted(() => {
+function initSound() {
+  return new Promise<Howl>((resolve) => {
+    // Setup the new Howl.
+    const _sound = new Howl({
+      src: ['/sound/1672977593140.mp3'],
+      onload: () => {
+        console.log('sound loaded.')
+        resolve(_sound)
+      }
+    })
+  });
+}
+
+let sound: any = null
+
+
+
+// Change global volume.
+Howler.volume(0.5);
+
+onMounted(async () => {
   console.log("Hello World")
   initSpine()
+
+  // Play the sound.
+  sound = await initSound()
+  console.log("sound: ", sound)
+
 })
 const player: any = ref(null)
 
@@ -13,59 +39,75 @@ function initSpine() {
   const playerInstance = new spine.SpinePlayer("player-container", {
     jsonUrl: "/pirate/pirate.json",
     atlasUrl: "/pirate/pirate.atlas",
-    // audioFolderPath: "/pirate/audio",
     // premultipliedAlpha: false,
-    // animation: "idle",
-    animations: ['idle', "attack"],
-    showControls: true,
+    animation: "idle",
+    showControls: false,
     backgroundColor: "#00000000",
     alpha: true,
     // Added:
-    // success: (player: any) => {
-    //   console.log('success: ', player)
-    //   player.setAnimation("attack", true)
-    //   player.animationState.setAnimation(0, "attack");
-    //   player.animationState.addListener({
-    //     event(t: any) {
-    //       console.log('addListener: ', t.animation.name)
-    //       switch (t.animation.name) {
-    //         case "attack":
-    //           player.animationState.setAnimation(0, "attack");
-    //           break;
-    //         case "idle":
-    //           player.animationState.setAnimation(0, "idle", true);
-    //           break;
-    //       }
-    //     }
-    //   })
-    // }
+    success: (player: any) => {
+      console.log('success: ', player)
+      player.animationState.addListener({
+        event(trackEntry: any, event: any) {
+          console.log('animationState event event: ', event)
+          console.log('animationState event trackEntry: ', trackEntry)
+          switch (event.data.name) {
+            case "music_Have":
+              sound.play()
+              break;
+            case "music_noHave":
+              sound.stop()
+              break;
+          }
+        },
+        complete(trackEntry: any) {
+          console.log('animationState complete trackEntry: ', trackEntry)
+          console.log('animationState complete trackEntry.animation.name: ', trackEntry.animation.name)
+          sound.stop()
+        },
+        start(trackEntry: any) {
+          console.log('animationState start trackEntry: ', trackEntry)
+          console.log('animationState start trackEntry.animation.name: ', trackEntry.animation.name)
+        }
+      })
+    }
   })
-  // setTimeout(() => {
-  //   playerInstance.setAnimation("attack", true)
-  //   playerInstance.animationState?.setAnimation(0, "attack", true)
-
-  // }, 2000);
-  // playerInstance.setAnimation('idle', true)
   player.value = playerInstance
 }
+
 function attack() {
   console.log('chage to attack')
   player.value.setAnimation("attack")
 }
+function idle() {
+  console.log('chage to idle')
+  player.value.setAnimation("idle")
+}
 function stop() {
   console.log('stop')
   player.value.animationState.setEmptyAnimation(0);
+}
+function playSound() {
+  sound.play()
 }
 </script>
 
 <template>
   <div class="bg-red-500">
     <div id="player-container" />
-    <button class="rounded bg-blue-400 p-2" @click="attack()">
-      player.setAnimation('attack')
-    </button>
-    <button class="rounded bg-blue-400 p-2" @click="stop()">
-      stop
-    </button>
+    <div class="flex gap-2 items-center justify-center">
+      <button class="rounded bg-blue-400 p-2" @click="attack()">
+        attack
+      </button>
+      <button class="rounded bg-blue-400 p-2" @click="idle()">
+        idle
+      </button>
+      <button class="rounded bg-blue-400 p-2" @click="stop()">
+        stop
+      </button>
+      <button class="rounded bg-blue-400 p-2" @click="playSound()">
+        play sound
+      </button>
+    </div>
   </div>
 </template>
